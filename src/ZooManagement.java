3db2e9d68d7e9d9fbcd5bd063181d8ddffa5211a
ahmedmazigh <am.ahmed.mazigh@gmail.com>
2025-01-21 16:42:11 +0100

    Initial commit

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -3,20 +3,18 @@ import java.util.*;
 public class ZooManagement {
     public static void main(String[] args) {
 
-        String nbrCages;
+        int nbrCages;
         String zooName;
 
         Scanner reader = new Scanner(System.in);
         System.out.println("Entrer nom zoo: ");
         zooName = reader.next();
-        do {
-            System.out.println("Entrer nbr cages: ");
-            nbrCages = reader.next();
+        System.out.println("Entrer nbr cages: ");
+        while(!reader.hasNextInt()) {
+            System.out.println(" Valeur invalide");
+            reader.next();
         }
-        while(!nbrCages.matches("[0-9]+"));
-
-        // System.out.println("Entrer nbr cages: ");
-        // int nbrCages = reader.nextInt();
+        nbrCages = reader.nextInt();
 
         System.out.println(zooName + " comporte " + nbrCages + " cages.");
     }
